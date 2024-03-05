a2cf2b537fbef7b1e834a1572ea3dbf50277eb78
Rak-Kyoung <rakk4403@gmail.com>
2024-03-05 22:48:58 +0900

    패키지 종속성 업데이트 및 Google Analytics 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import ReactGA from "react-ga";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,28 +16,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Google Analytics ID를 여기에 입력하세요
-    ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID || "");
-
-    // 페이지뷰를 추적하는 함수
-    const handleRouteChange = (url: string) => {
-      ReactGA.pageview(url);
-    };
-
-    // 라우트 변경 시 이벤트 리스너를 추가
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    // 컴포넌트 언마운트 시 이벤트 리스너를 제거
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
     </html>
   );
 }
