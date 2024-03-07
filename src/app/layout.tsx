ace14ffac622ca3f10786d67019771e1fc028199
Rak-Kyoung <rakk4403@gmail.com>
2024-03-07 20:37:51 +0900

    layout.tsx 파일에 메타데이터 및 헤드 태그 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Continent by Country</title>
+        <meta
+          name="description"
+          content="Enter country names and find their continents. If the country name is
+          not found, select from the list of similar names."
+        />
+        <meta property="og:title" content="Continent by Country" />
+        <meta
+          property="og:description"
+          content="Enter country names and find their continents. If the country name is
+          not found, select from the list of similar names."
+        />
+        {/* <meta property="og:image" content="공유될 때 표시될 이미지 URL" /> */}
+        <meta property="og:url" content="country-csv.vercel.app" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <body className={inter.className}>{children}</body>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
     </html>
